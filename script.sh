b1e1d5a8fce5573f05f6af41ec3a92f9fff1b102
Monty <tecnisoft01@gmail.com>
2023-03-26 18:57:54 -0300

    Install Python Pack

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 
 # Instalar paquetes solicitados
 sudo apt-get install -y apache2 vim curl telnet unzip wget net-tools htop nmap nginx
-
+sudo apt install python-pip python-dev libmysqlclient-dev
 # Reiniciar servicio de Nginx
 sudo service nginx restart
 
@@ -14,3 +14,6 @@ sudo useradd -m -s /bin/bash monty
 
 # Senha do usuário
 echo "monty:senha123" | sudo chpasswd
+
+# Reboot
+sudo reboot now
